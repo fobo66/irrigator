cf64573c0c128ee8f55e3b6c2b08fd932d6a4ad5
Andrey Mukamolov <fobo66@protonmail.com>
2020-02-22 00:28:56 +0300

    Clean up sketch

--- irrigator.ino
+++ irrigator.ino
@@ -1,44 +1,32 @@
-#define POMP_PIN        13
+#define PUMP_PIN        13
 #define HUMIDITY_PIN    A0
-// минимальный порог влажности почвы
 #define HUMIDITY_MIN    200
-// максимальный порог влажности почвы
-#define HUMIDITY_MAX    800
-// интервал между проверкой на полив растения
-#define INTERVAL        60000 * 3   
-// переменная для хранения показания влажности почвы
+
+// interval between humidity probes
+#define INTERVAL        60000 * 3
+#define PUMP_TIME       2000
+
 unsigned int humidity = 0;
 
-// статическая переменная для хранения времени
 unsigned long waitTime = 0;
 
 void setup(void)
 {
-  // пин помпы в режим выхода
-  pinMode(POMP_PIN, OUTPUT);
+  pinMode(PUMP_PIN, OUTPUT);
 }
 
 void loop(void)
 {
-  // считываем текущее показания датчика влажности почвы
   int humidityNow = analogRead(HUMIDITY_PIN);
-  // если показания текущей влажности почвы
-  // не равняется предыдущему запросу 
+  
   if(humidityNow != humidity) {
-    // сохраняем текущие показания влажности
     humidity= humidityNow;
   }
-  // если прошёл заданный интервал времени
-  // и значения датчика влажности меньше допустимой границы
+
   if ((waitTime == 0 || millis() - waitTime > INTERVAL) && humidity < HUMIDITY_MIN ) {
-    // включаем помпу
-    digitalWrite(POMP_PIN, HIGH);
-    // ждём 2 секунды
-    delay(2000);
-    // выключаем помпу
-    digitalWrite(POMP_PIN, LOW);
-    // приравниваем переменной waitTime
-    // значение текущего времени плюс 3 минуты 
+    digitalWrite(PUMP_PIN, HIGH);
+    delay(PUMP_TIME);
+    digitalWrite(PUMP_PIN, LOW);
     waitTime = millis();
   }
 }
